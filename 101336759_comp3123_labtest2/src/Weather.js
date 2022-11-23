3ac731834275edff7309293b0e3c2bc0b2593fec
Phoenix Armand Ani <PhoenixArmand30@outlook.com>
2022-11-23 18:50:20 -0500

    Bruh

---
+++ 101336759_comp3123_labtest2/src/Weather.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+import { useEffect,useState } from "react";
+let Weather = () => {
+    let [weather,setWeather] = useState();
+    let getWeather = () =>{
+        axios.get('https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=051c11a296f44220c98d0c7d3abce3b8')
+        .then((res)=>{
+            setWeather(res.data);
+            console.log(res.data);
+        })
+    }
+    useEffect(()=>{
+        getWeather();
+    },[])
+    let weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][new Date().getDay()]
+    
+    console.log(weekday);
+    return(
+        <div style={{display:'flex',justifyContent:'center',marginTop:40,color:'white'}}>
+        
+        <div style={{width:400,height:500,backgroundColor:'#5594fa'}}>
+        <h4> Weather App </h4>
+        <hr/>
+           <div style={{marginTop:20}}>
+            {weekday}
+           </div>
+           <div style={{marginTop:20}}>
+           <img src={`https://openweathermap.org/img/wn/${weather?.weather[0]?.icon}@2x.png`}/>
+           <br/>
+           <h1>
+           {(weather?.main?.temp -273.15).toFixed(2)}°C
+           </h1>
+           <h3>
+           {weather?.name}
+           </h3>
+           Description: {weather?.weather[0]?.description} 
+           </div>
+           <div style={{marginTop:20}}>
+            Lng:{weather?.coord?.lon} Lat:{weather?.coord?.lat}
+           </div>
+           <div style={{marginTop:20}}>
+            Wind Speed: {weather?.wind?.speed} &nbsp; &nbsp; &nbsp; &nbsp; Gust:{weather?.wind?.gust}
+           </div>
+           <div style={{marginTop:40}}>
+            Mon
+           </div>
+        </div>
+        </div>
+    )
+}
+
+export default Weather;
